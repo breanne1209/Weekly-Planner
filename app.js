bcb20afb50266e4656e96355c9ad4f0e41822de9
breanne1209 <128068942+breanne1209@users.noreply.github.com>
2023-07-09 12:02:24 -0400

    Adjusted scroll function

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
-const inputBox = document.getElementById('input-area');
-const listContainer = document.getElementById('list');
+const clickPlus = () => {
+  document.querySelectorAll('.day button').forEach(el => {
+    el.addEventListener('click', evt => {
+      evt.stopImmediatePropagation();
+      const el_day = el.closest('.day');
+      const val = el_day.querySelector('input').value;
+      if (val === '') {
+        alert('Please enter a valid value :)');
+      } else {
+        let li = document.createElement('li');
+        li.innerHTML = val;
+        el_day.querySelector('ul').appendChild(li);
 
-function addTask() {
-  if(inputBox.value === '') {
-    alert('Please enter a valid value :)')
-  }
-  else {
-    let li = document.createElement('li');
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    let span = document.createElement('span');
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
-  }
-  inputBox.value = '';
-}
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        span.addEventListener('click', (e) => {
+          e.stopPropagation();
+          e.target.parentElement.remove();
+          checkScroll(); // Call checkScroll whenever an LI is removed
+        });
 
-listContainer.addEventListener('click', function(e) {
-  if(e.target.tagName === 'LI'){
-      e.target.classList.toggle('checked');
-  }
-  else if(e.target.tagName === 'SPAN'){
-      e.target.parentElement.remove();
-  }
-}, false);
+        li.appendChild(span);
+
+        li.addEventListener('click', () => {
+          li.classList.toggle('checked');
+        });
+
+        checkScroll(); // Call checkScroll whenever an LI is added
+      }
+      el_day.querySelector('input').value = '';
+    });
+  });
+};
+
+const checkScroll = () => {
+  const dayElements = document.querySelectorAll('.day');
+  const threshold = 4;
+
+  dayElements.forEach(dayElement => {
+    const liElements = dayElement.querySelectorAll('li');
+
+    if (liElements.length > threshold) {
+      dayElement.classList.add('scrollable');
+    } else {
+      dayElement.classList.remove('scrollable');
+    }
+  });
+};
+
+window.addEventListener('load', clickPlus);
